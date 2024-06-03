a5a1f802df35a28a8a295a30bd476e84fde7a27a
gciaburri <153664777+gciaburri@users.noreply.github.com>
2024-06-03 18:12:40 -0500

    started implementing ExercisesView

--- fitnessapp/Weights/ExercisesView.swift
+++ fitnessapp/Weights/ExercisesView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct ExercisesView: View {
+    let exercises: [Exercise]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(exercise, id: \.title) { exercise in
+            ExerciseCardView(exercise: exercise)
+
     }
 }
 
 #Preview {
-    ExercisesView()
+    ExercisesView(exercises: Exercise.sampleData)
 }
