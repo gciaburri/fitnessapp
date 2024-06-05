d78a1707967677856facfe853f47f0ec555f3933
gciaburri <gabriel@ciaburribrand.com>
2024-06-05 00:18:04 -0500

    implemented onChange to attempt to fix the issue of ExerciseDetailView not loading correctly

--- fitnessapp/Weights/ExercisesView.swift
+++ fitnessapp/Weights/ExercisesView.swift
@@ -11,25 +11,51 @@ struct ExercisesView: View {
     let exercises: [Exercise]
     @State private var selectedExercise: Exercise? // Optional Exercise
     @State private var showingDetail = false
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack {
-            List(exercises, id: \.title) { exercise in
-                Button(action: {
-                    selectedExercise = exercise
-                    showingDetail = true
-                }) {
-                    ExerciseCardView(exercise: exercise)
-                        .padding(.vertical, 0)
-                        .listRowInsets(EdgeInsets())
+            List {
+                if searchResults.isEmpty {
+                    Text("No exercises found")
+                        .foregroundColor(.gray)
+                        .padding()
+                } else {
+                    ForEach(searchResults, id: \.title) { exercise in                                     Button(action: {
+                            selectedExercise = exercise
+                            showingDetail = true
+                    }) {
+                        ExerciseCardView(exercise: exercise)
+                            .padding(.vertical, 0)
+                            .listRowInsets(EdgeInsets())
+                    }
                 }
             }
         }
-        .navigationTitle("Exercises")
-        .sheet(isPresented: $showingDetail) {
-            if let selectedExercise = selectedExercise {
-                ExerciseDetailView(exercise: selectedExercise)
+            .navigationTitle("Exercises")
+            .searchable(text: $searchText,
+                        placement: .navigationBarDrawer(displayMode: .always))
+            .sheet(isPresented: $showingDetail, onDismiss: {
+                selectedExercise = nil
+            }) {
+                if let selectedExercise = selectedExercise {
+                    NavigationStack {
+                        ExerciseDetailView(exercise: selectedExercise)
+                    }
+                }
             }
+            .onChange(of: selectedExercise) { newValue, oldValue in
+                        showingDetail = newValue != nil
+                    }
+        }
+    }
+    
+    
+    var searchResults: [Exercise] {
+        if searchText.isEmpty {
+            return exercises
+        } else {
+            return exercises.filter { $0.title.contains(searchText) }
         }
     }
 }
