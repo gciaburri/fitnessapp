1aca1dc31dcf072d5f30c176f9f1f45cca174b1b
gciaburri <gabriel@ciaburribrand.com>
2024-06-06 00:55:23 -0500

    refactored Exercises to allow editing by user. TODO: fix Binding and State transitions between ExercisesView, DetailView, and EditView

---
+++ fitnessapp/Weights/ExerciseDetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseDetailEditView.swift
+//  fitnessapp
+//
+//  Created by Gabriel Ciaburri on 6/5/24.
+//
+
+import SwiftUI
+
+struct ExerciseDetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseDetailEditView()
+}
